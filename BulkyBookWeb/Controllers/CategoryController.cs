24cc331b838da0997f8da5c11609dbbef396d1cb
David Suarez <david@dsbtsoftware.com>
2022-03-15 19:48:54 -0400

    edit is now working

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -45,5 +45,42 @@ namespace BulkyBookWeb.Controllers
             }
             return View(obj);
         }
+
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order and Name must be different.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Attach(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
