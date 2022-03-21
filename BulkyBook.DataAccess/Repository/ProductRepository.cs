5bef5cda2d55d0910d43d5edbae0bb43eb183e05
David Suarez <david@dsbtsoftware.com>
2022-03-21 19:50:33 -0400

    Product

---
+++ BulkyBook.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using BulkyBook.DataAccess.Repository.iRepository;
+using BulkyBook.Models;
+using BulkyBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext Db) : base(Db)
+        {
+            _db = Db;
+
+        }
+
+
+        public void Update(Product product)
+        {
+            _db.Products.Update(product);
+        }
+    }
+}
