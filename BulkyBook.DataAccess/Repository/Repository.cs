fb8a0d993b7610538f99e0f234b69e0075070240
David Suarez <david@dsbtsoftware.com>
2022-03-21 11:16:40 -0400

    ready to create the model repo now

---
+++ BulkyBook.DataAccess/Repository/Repository.cs
@@ -0,0 +1,56 @@
+﻿using BulkyBook.DataAccess.Repository.iRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext Db)
+        {
+            _db = Db;
+            this.dbSet = _db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+
+            query = query.Where(filter);
+
+#pragma warning disable CS8603 // Possible null reference return.
+            return query.FirstOrDefault();
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(T);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
