4aa11c03424fb4dff6ab73207a9812d9e6ca6b48
David Suarez <david@dsbtsoftware.com>
2022-03-21 11:22:06 -0400

    interface is ready

---
+++ BulkyBook.DataAccess/Repository/iRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository.iRepository
+{
+    public interface ICategoryRepository :IRepository<Category>
+    {
+        void update(Category category);
+
+        void Save();
+    }
+}
