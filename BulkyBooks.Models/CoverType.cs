80b3c90eb3622d786e8e8e699b41fc0e30ed0b34
David Suarez <david@dsbtsoftware.com>
2022-03-21 13:36:29 -0400

    restrict name size

--- BulkyBooks.Models/CoverType.cs
+++ BulkyBooks.Models/CoverType.cs
@@ -13,6 +13,8 @@ namespace BulkyBooks.Models
         public int Id { get; set; }
 
         [Required]
+        [MaxLength(50)]
+        [Display(Name ="Cover Type")]
         public string Name { get; set; }
     }
 }
