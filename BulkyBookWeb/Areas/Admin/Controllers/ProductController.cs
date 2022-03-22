16d1bad485a0346772d88ae3c93543d8ffc4825d
David Suarez <david@dsbtsoftware.com>
2022-03-22 15:32:52 -0400

    done

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -134,7 +134,7 @@ namespace BulkyBookWeb.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var productList = _unitOfWork.Product.GetAll();
+            var productList = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
 
             return Json(new {data = productList});
         }
