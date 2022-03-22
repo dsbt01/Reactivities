b16a2efaaa93bdc5036f3e03013be169cf86d0f9
David Suarez <david@dsbtsoftware.com>
2022-03-22 15:46:00 -0400

    ready to edit

--- BulkyBookWeb/wwwroot/js/product.js
+++ BulkyBookWeb/wwwroot/js/product.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": {
-            "url":"/Admin/Product/GetAll"
+            "url": "/Admin/Product/GetAll"
         },
         "columns": [
             { "data": "title", "width": "15%" },
@@ -16,6 +16,18 @@ function loadDataTable() {
             { "data": "author", "width": "15%" },
             { "data": "category.name", "width": "15%" },
             { "data": "coverType.name", "width": "15%" },
+            {
+                "data": "id",
+                "width": "15%",
+                "render": function (data) {
+                    return `
+                        <div class="w-75 btn-group" role="group">
+                            <a href="/Admin/Product/Upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i>Edit</a>
+                            <a class="btn btn-danger mx-2"><i class="bi bi-trash3-fill"></i>Delete</a>
+                        </div>
+                    `
+                }
+            },
         ]
     });
 }
