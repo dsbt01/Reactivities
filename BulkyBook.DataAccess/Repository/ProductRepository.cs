f930c790563517c94c70a883627db670743f61a6
David Suarez <david@dsbtsoftware.com>
2022-03-22 16:44:56 -0400

    it is working now

--- BulkyBook.DataAccess/Repository/ProductRepository.cs
+++ BulkyBook.DataAccess/Repository/ProductRepository.cs
@@ -22,27 +22,6 @@ namespace BulkyBook.DataAccess.Repository
 
         public void Update(Product product)
         {
-            var objFromDb = _db.Products.FirstOrDefault(r => r.Id == product.Id);
-
-            if (objFromDb != null)
-            {
-                objFromDb.Title = product.Title;
-                objFromDb.ISBN = product.ISBN;
-                objFromDb.Price = product.Price;
-                objFromDb.Price50 = product.Price50;
-                objFromDb.ListPrice = product.ListPrice;
-                objFromDb.Price100 = product.Price100;
-                objFromDb.Description = product.Description;
-                objFromDb.CategoryId = product.CategoryId;
-                objFromDb.Author = product.Author;
-                objFromDb.CoverTypeId = product.CoverTypeId;
-
-                if (product.ImageUrl != null)
-                {
-                    objFromDb.ImageUrl = product.ImageUrl;
-                }
-            }
-
             _db.Products.Update(product);
         }
     }
