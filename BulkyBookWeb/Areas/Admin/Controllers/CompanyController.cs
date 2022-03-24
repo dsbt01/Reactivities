71720146419bc15c3fd7c824b2fbd7d3cdc27814
David Suarez <david@dsbtsoftware.com>
2022-03-24 10:14:51 -0400

    Company controller

---
+++ BulkyBookWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,170 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository.iRepository;
+using BulkyBook.Models;
+using BulkyBooks.Models;
+using BulkyBooks.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyBookWeb.Controllers
+{
+    [Area("Admin")]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CompanyController(IUnitOfWork UnitOfWork)
+        {
+            _unitOfWork = UnitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        //Get
+        public IActionResult Upsert(int? id)
+        {
+            ProductVM productVM = new()
+            {
+                Product = new(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
+            };
+
+            if (id == null || id == 0)
+            {
+                //create product
+                //ViewBag.CategoryList = productVM.CategoryList;
+                //ViewData["CoverTypeList"] = productVM.CoverTypeList;
+
+                return View(productVM);
+            }
+            else
+            {
+                //Update product
+
+                productVM.Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+                return View(productVM);
+            }
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductVM obj, IFormFile? file)
+        {
+            if (ModelState.IsValid)
+            {
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString();
+
+                    var uploads = Path.Combine(wwwRootPath, @"images\products");
+                    var extension = Path.GetExtension(file.FileName);
+
+                    //delete old image first
+                    if (obj.Product.ImageUrl != null)
+                    {
+                        var oldImagepath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagepath))
+                        {
+                            System.IO.File.Delete(oldImagepath);
+
+                        }
+                    }
+
+                    using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                    {
+                        file.CopyTo(fileStreams);
+                    }
+
+                    obj.Product.ImageUrl = @"\images\products\" + fileName + extension;
+                }
+
+                if (obj.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(obj.Product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(obj.Product);
+                }
+
+
+                _unitOfWork.Save();
+
+                TempData["success"] = "Product Created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+               
+
+        //post
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _unitOfWork.Product.GetFirstOrDefault(c => c.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+
+            TempData["success"] = "Product deleted successfully";
+            return RedirectToAction("Index");
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+
+            return Json(new { data = productList });
+        }
+
+        //post
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var obj = _unitOfWork.Product.GetFirstOrDefault(c => c.Id == id);
+            if (obj == null)
+            {
+                return Json(new { success = false, message = "Error while deleting. Not found" });
+            }
+
+            string wwwRootPath = _hostEnvironment.WebRootPath;
+
+            var oldImagepath = Path.Combine(wwwRootPath, obj.ImageUrl.TrimStart('\\'));
+            if (System.IO.File.Exists(oldImagepath))
+            {
+                System.IO.File.Delete(oldImagepath);
+
+            }
+
+
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+    }
+}
