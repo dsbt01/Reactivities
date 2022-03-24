924c03e192a942a848aa4018df8ad13da45d1b22
David Suarez <david@dsbtsoftware.com>
2022-03-24 13:49:29 -0400

    fix it

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
 //builder.Services.AddDefaultIdentity<IdentityUser>()
 //    .AddEntityFrameworkStores<ApplicationDbContext>();
 
+builder.Services.AddRazorPages();
+
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
