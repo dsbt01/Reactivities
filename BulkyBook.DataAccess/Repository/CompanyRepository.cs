7b8c6d891e6435fed3f0b2dc8075e01ed6c2716d
David Suarez <david@dsbtsoftware.com>
2022-03-24 10:05:49 -0400

    completed company repo

---
+++ BulkyBook.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,28 @@
+﻿using BulkyBook.DataAccess.Repository.iRepository;
+using BulkyBook.Models;
+using BulkyBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private ApplicationDbContext _db;
+
+        public CompanyRepository(ApplicationDbContext Db) : base(Db)
+        {
+            _db = Db;
+
+        }
+
+
+        public void Update(Company company)
+        {
+            _db.Company.Update(company);
+        }
+    }
+}
