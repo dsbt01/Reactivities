70c70d5c83ba3f2f29429ee48392e5a6e46c4cb9
David Suarez <david@dsbtsoftware.com>
2022-03-24 13:41:17 -0400

    Commit

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -42,6 +42,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
