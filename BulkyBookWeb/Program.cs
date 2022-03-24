b583084f0e3c6228e3866aa2cdd75db6c9ff6411
David Suarez <david@dsbtsoftware.com>
2022-03-24 09:04:42 -0400

    Now it is ready

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -12,12 +12,11 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-
 
 var app = builder.Build();
 
