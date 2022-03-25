ae87e0163ee1f8bfff2adb65e53010ace4edf68c
David Suarez <dsuarez001@att.net>
2022-03-25 18:32:21 -0400

    ApplicationUser repo is ready

---
+++ BulkyBook.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,28 @@
+﻿using BulkyBook.DataAccess.Repository.iRepository;
+using BulkyBook.Models;
+using BulkyBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ApplicationUserRepository(ApplicationDbContext Db) : base(Db)
+        {
+            _db = Db;
+
+        }
+
+
+        public void Update(ApplicationUser applicationUser)
+        {
+            _db.ApplicationUsers.Update(applicationUser);
+        }
+    }
+}
