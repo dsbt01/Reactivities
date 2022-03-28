9f0341b95621ba75437a7d8e867fd1e7da5cf1c3
David Suarez <dsuarez001@att.net>
2022-03-28 18:25:59 -0400

    Total is done

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -36,6 +36,7 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
             foreach (var cart in ShoppingCartVM.ListCart)
             {
                 cart.Price = GetPriceBasedOnQuantity(cart.Count, cart.Product.Price, cart.Product.Price50, cart.Product.Price100);
+                ShoppingCartVM.CartTotal += (cart.Price * cart.Count);
             }
 
 
