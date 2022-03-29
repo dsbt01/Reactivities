9b584ad09de1aa3f969009f49ba7e063b238c645
David Suarez <dsuarez001@att.net>
2022-03-29 13:46:24 -0400

    Status created

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -115,6 +115,33 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
         }
 
 
+        [HttpPost]
+        [ActionName("Summary")]
+        [ValidateAntiForgeryToken]
+        public IActionResult SummaryPOST()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+
+            ShoppingCartVM.ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value, includeProperties: "Product");
+
+            //ShoppingCartVM.OrderHeader.PaymentStatus = 
+
+
+            foreach (var cart in ShoppingCartVM.ListCart)
+            {
+                cart.Price = GetPriceBasedOnQuantity(cart.Count, cart.Product.Price, cart.Product.Price50, cart.Product.Price100);
+                ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+            }
+
+
+            //return View(ShoppingCartVM);
+
+            return View();
+        }
+
+
 
         /// <summary>Gets the price based on quantity.</summary>
         /// <param name="quantity">The quantity.</param>
