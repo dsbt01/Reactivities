5ad90c590c5e35e8efdfd08100ebaba3731d10be
David Suarez <dsuarez001@att.net>
2022-03-30 19:24:26 -0400

    order detail is coming up

--- BulkyBookWeb/wwwroot/js/order.js
+++ BulkyBookWeb/wwwroot/js/order.js
@@ -10,19 +10,19 @@ function loadDataTable() {
             "url": "/Admin/Order/GetAll"
         },
         "columns": [
-            { "data": "id", "width": "15%" },
-            { "data": "name", "width": "15%" },
+            { "data": "id", "width": "5%" },
+            { "data": "name", "width": "25%" },
             { "data": "phoneNumber", "width": "15%" },
             { "data": "applicationUser.email", "width": "15%" },
             { "data": "orderStatus", "width": "15%" },
-            { "data": "orderTotal", "width": "15%" },
+            { "data": "orderTotal", "width": "10%" },
             {
                 "data": "id",
-                "width": "15%",
+                "width": "5%",
                 "render": function (data) {
                     return `
                         <div class="w-75 btn-group" role="group">
-                            <a href="/Admin/Order/Details?orderId=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Details</a>
+                            <a href="/Admin/Order/Details?orderId=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i></a>
                         </div>
                     `
                 }
