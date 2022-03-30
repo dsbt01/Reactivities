96aa5568ded793f712f44c615c941ce304032a8c
David Suarez <dsuarez001@att.net>
2022-03-30 19:16:12 -0400

    order js is ready

---
+++ BulkyBookWeb/wwwroot/js/order.js
@@ -0,0 +1,32 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+})
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Order/GetAll"
+        },
+        "columns": [
+            { "data": "id", "width": "15%" },
+            { "data": "name", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "applicationUser.email", "width": "15%" },
+            { "data": "orderStatus", "width": "15%" },
+            { "data": "orderTotal", "width": "15%" },
+            {
+                "data": "id",
+                "width": "15%",
+                "render": function (data) {
+                    return `
+                        <div class="w-75 btn-group" role="group">
+                            <a href="/Admin/Order/Details?orderId=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Details</a>
+                        </div>
+                    `
+                }
+            },
+        ]
+    });
+}
