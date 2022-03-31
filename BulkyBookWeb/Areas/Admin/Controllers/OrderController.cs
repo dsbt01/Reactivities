03029ba9b02b7623a70cbb660536fe8867a869ff
David Suarez <dsuarez001@att.net>
2022-03-30 20:10:47 -0400

    filter is done

--- BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,6 +1,8 @@
 ﻿using BulkyBook.DataAccess.Repository.iRepository;
+using BulkyBook.Utility;
 using BulkyBooks.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
@@ -21,10 +23,30 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
 
         #region API CALLS
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
             IEnumerable<OrderHeader> orderHeaders;
             orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaders = orderHeaders.Where(x => x.PaymentIntentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+
+                case "inprocess":
+                    orderHeaders = orderHeaders.Where(x => x.OrderStatus == SD.StatusInProcess);
+                    break;
+
+                case "completed":
+                    orderHeaders = orderHeaders.Where(x => x.OrderStatus == SD.StatusShipped);
+                    break;
+
+                default:
+                    break;
+            }
+
+
             return Json(new { data = orderHeaders });
         }
 
