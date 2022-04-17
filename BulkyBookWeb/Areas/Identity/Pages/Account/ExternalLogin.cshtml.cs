8c424df1dd0b867ee22bd41efe5ac4c2a615b1f7
David Suarez <dsuarez001@att.net>
2022-04-17 12:19:03 -0400

    Update ExternalLogin.cshtml.cs

--- BulkyBookWeb/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ BulkyBookWeb/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -88,13 +88,13 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
             [Required]
             public string Name { get; set; }
 
-            public string StreetAddress { get; set; }
+            public string? StreetAddress { get; set; }
 
-            public string City { get; set; }
+            public string? City { get; set; }
 
-            public string State { get; set; }
+            public string? State { get; set; }
 
-            public string PostalCode { get; set; }
+            public string? PostalCode { get; set; }
 
             public string? PhoneNumber { get; set; }
 
