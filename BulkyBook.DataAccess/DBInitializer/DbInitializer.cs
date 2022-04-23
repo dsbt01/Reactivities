522eddb0fd91af0af1150ede34ce6a257695f291
David Suarez <dsuarez001@att.net>
2022-04-23 12:30:22 -0400

    ready to code the initializer

---
+++ BulkyBook.DataAccess/DBInitializer/DbInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.DBInitializer
+{
+	public class DbInitializer : IDbInitializer
+	{
+		public void Initialize()
+		{
+			//apply migrations
+
+			//create roles if not created
+
+			//make sure admin role is create if needed
+
+
+		}
+	}
+}
