97995899bd52589a870ac97d6090be692ff8c627
David Suarez <dsuarez001@att.net>
2022-04-23 14:50:35 -0400

    Ready to call the new code

--- BulkyBook.DataAccess/DBInitializer/DbInitializer.cs
+++ BulkyBook.DataAccess/DBInitializer/DbInitializer.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using BulkyBook.DataAccess.Repository.iRepository;
+using BulkyBook.Utility;
+using BulkyBooks.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,17 +12,65 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.DataAccess.DBInitializer
 {
-	public class DbInitializer : IDbInitializer
-	{
-		public void Initialize()
-		{
-			//apply migrations
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _db;
 
-			//create roles if not created
+        public DbInitializer(
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            ApplicationDbContext db)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _db = db;
+        }
 
-			//make sure admin role is create if needed
 
+        public void Initialize()
+        {
+            //apply migrations
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
 
-		}
-	}
+            //create roles if not created
+            if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_User_Indi)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_User_Comp)).GetAwaiter().GetResult();
+
+                //Make sure you create the default admin user
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "dsuarez001@att.net",
+                    Email = "dsuarez001@att.net",
+                    Name = "David A Suarez",
+                    PhoneNumber = "3057757396",
+                    StreetAddress = "22863 SW 113Th PATH",
+                    State = "FL",
+                    PostalCode = "33170",
+                    City = "MIAMI"
+                }, "Infinity_2020").GetAwaiter().GetResult();
+
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "dsuarez001@att.net");
+
+                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+            }
+
+            return;
+        }
+    }
 }
